61dae60c1fa8f62b5b2fd74696fac50e24316a2b
ArtemyevArtyom <gof.ru@yandex.ru>
2022-10-06 23:42:19 +0400

    Homework 2 C#

---
+++ task13/Program.cs
@@ -0,0 +1,42 @@
+﻿/* 
+Задача 13: Напишите программу, которая выводит третью цифру
+заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int getTheDigit(int minBorder, int maxBorder)
+{
+    int result = new int();
+    result = new Random().Next(minBorder, maxBorder);
+    return result;
+}
+
+int getTheThirdDigitFromNumber(int number)
+{
+    int currentDigit = new int();
+        if (9999 < number)
+        {
+            currentDigit = number / 100;
+        }
+        else if (999 < number && 10000 > number)
+        {
+            currentDigit = number / 10;
+        }
+        else if (number < 100)
+        {
+        Console.WriteLine("Третьей цифры нет");
+        }
+        else if (number > 100 && number < 1000)
+        {
+        currentDigit = number;
+        }
+    int thirdDigits = currentDigit % 10;
+    return thirdDigits;
+}
+
+int RandomNumber = getTheDigit(0, 100000);
+Console.WriteLine($"Заданное число {RandomNumber}");
+int thirdDigit = getTheThirdDigitFromNumber (RandomNumber);
+Console.WriteLine($"У числа {RandomNumber} третья цифра {thirdDigit}");
---
+++ task15/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int getTheDayOfWeek(int minBorder, int maxBorder)
+{
+    int result = new int();
+    result = new Random().Next(minBorder, maxBorder);
+    return result;
+}
+
+int checkTheDayOfWeek(int number)
+{
+    if(number > 5) 
+    {
+            Console.WriteLine("Да, выходной");
+            return(number);
+    }
+    else 
+    {
+        Console.WriteLine("Не выходной");
+        return(number);
+    }
+}
+
+int theWeekDay = getTheDayOfWeek(1, 8);
+int Weekend = checkTheDayOfWeek(theWeekDay);
+Console.WriteLine($"День недели: {theWeekDay}");
